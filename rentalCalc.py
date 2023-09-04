e83616cf0c4e922e8139942e69b6c2132452303c
Dylan Smith <dysm8245@colorado.edu>
2023-09-04 00:12:53 -0600

    first commit

---
+++ rentalCalc.py
@@ -0,0 +1,53 @@
+class Income():
+
+    def __init__(self, rentInc):
+        self.rentalIncome = rentInc
+        self.laundry = 0
+        self.storage = 0
+        self.misc = 0
+
+    def incomeCalc(self):
+        income = 0
+    
+        return income
+    
+class Expenses():
+
+    def __init__(self):
+        self.tax = 0
+        self.insurance = 0
+        self.utilities = 0
+        self.repairs = 0
+        self.capEx = 0
+        self.vacancy = 0
+        self.propManage = 0
+        self.mortgage = 0
+
+    def expenseCalc(self):
+        expense = 0
+
+        return expense
+    
+class CashFlow():
+
+    def __init__(self):
+        self.income
+        self.expenses
+    
+    def calcFlow(self):
+        flow = 0
+
+        return flow
+    
+class ROI():
+
+    def __init__(self):
+        self.downPay = 0
+        self.closingCosts = 0
+        self.rehabBudget = 0
+        self.misc = 0
+
+    def getFlow():
+        
+
+    def calcROI(self):
